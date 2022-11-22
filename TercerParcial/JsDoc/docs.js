df162527ba4084bacfd0afadf8a632416d4867ae
BryanTurrubiates <bryanturrubiates@outlook.com>
2022-11-22 08:31:31 -0600

    Se subio JSDOCS

---
+++ TercerParcial/JsDoc/docs.js
@@ -0,0 +1,9 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} name - The title of the driver.
+ * @param {string} lastName - The author of the driver.
+ */
+export function Driver(name, lastName) {
+    console.log(`Driver ${name} ${lastName} created`);
+}
---
+++ TercerParcial/JsDoc/index.js
@@ -0,0 +1,4 @@
+import { Driver } from './docs.js';
+
+// Create a new driver
+const driver = new Driver('Lewis', 'Hamilton');
